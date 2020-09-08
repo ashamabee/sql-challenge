210f704edbefc043a6274a2bedf882ce0960d85d
ashamabee <65267655+ashamabee@users.noreply.github.com>
2020-09-07 20:31:53 -0700

    Just two more questions to go

--- EmployeeSQL/schema.sql
+++ EmployeeSQL/schema.sql
@@ -1,3 +1,4 @@
+-- Adding Tables
 CREATE TABLE employees (
     emp_no INTEGER NOT NULL PRIMARY KEY,
     emp_title VARCHAR(30) NOT NULL,
@@ -28,14 +29,52 @@ CREATE TABLE titles (
     title_id VARCHAR(30) NOT NULL PRIMARY KEY,
     title VARCHAR(30) NOT NULL
 );
-CREATE TABLE "salaries" (
-    "emp_no" INTEGER NOT NULL PRIMARY KEY,
-    "salary" INTEGER NOT NULL,
+CREATE TABLE salaries (
+    emp_no INTEGER NOT NULL PRIMARY KEY,
+    salary INTEGER NOT NULL,
 	FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
 );
 -- Add foreign key to employee table
 ALTER TABLE employees
 ADD FOREIGN KEY (emp_title) REFERENCES titles(title_id);
-
--- When I tried to import the employee data, I got this error:
--- ERROR:  insert or update on table "employees" violates foreign key constraint "employees_emp_title_fkey"
+-- View data
+SELECT * FROM dept_manager;
+-- List the following details of each employee: employee number, last name, first name, sex, and salary.
+SELECT e.emp_no, e.last_name, e.first_name, e.sex, s.salary
+FROM employees AS e
+INNER JOIN salaries AS s ON
+e.emp_no = s.emp_no;
+-- List first name, last name, and hire date for employees who were hired in 1986.
+SELECT first_name, last_name, hire_date
+FROM employees
+WHERE YEAR(hire_date) = 1986;
+----------------------can't get the year to work
+-- List the manager of each department with the following information: department number, department name, the manager's employee number, last name, first name.
+---- THERE ARE MORE THAN ONE MANAGER PER DEPARTMENT
+SELECT dm.dept_no, d.dept_name, dm.emp_no, e.last_name, e.first_name
+FROM dept_manager AS dm
+       JOIN employees AS e ON dm.emp_no = e.emp_no      
+       JOIN departments AS d ON dm.dept_no = d.dept_no;
+-- List the department of each employee with the following information: employee number, last name, first name, and department name.
+SELECT e.emp_no, e.last_name, e.first_name, d.dept_name
+FROM employees AS e
+       JOIN dept_emp AS de ON de.emp_no = e.emp_no      
+       JOIN departments AS d ON de.dept_no = d.dept_no;
+-- List first name, last name, and sex for employees whose first name is "Hercules" and last names begin with "B."
+SELECT first_name, last_name, sex
+FROM employees
+WHERE first_name = 'Hercules'
+AND last_name LIKE 'B%';
+-- List all employees in the Sales department, including their employee number, last name, first name, and department name.
+SELECT e.emp_no, e.last_name, e.first_name, d.dept_name
+FROM employees AS e
+       JOIN dept_emp AS de ON de.emp_no = e.emp_no      
+       JOIN departments AS d ON de.dept_no = d.dept_no
+	   WHERE d.dept_name = 'Sales';
+-- List all employees in the Sales and Development departments, including their employee number, last name, first name, and department name.
+SELECT e.emp_no, e.last_name, e.first_name, d.dept_name
+FROM employees AS e
+       JOIN dept_emp AS de ON de.emp_no = e.emp_no      
+       JOIN departments AS d ON de.dept_no = d.dept_no
+	   WHERE d.dept_name = 'Sales' OR d.dept_name = 'Development';
+-- In descending order, list the frequency count of employee last names, i.e., how many employees share each last name
